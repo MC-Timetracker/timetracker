27d36e2a7721f91da074ad191406f468d050beb8
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-10-11 23:56:05 +0530

    added interface of TaskSuggesters

---
+++ src/iiitd/mc/timetracker/context/ITaskSuggester.java
@@ -0,0 +1,29 @@
+package iiitd.mc.timetracker.context;
+
+import iiitd.mc.timetracker.data.Task;
+
+import java.util.List;
+
+/**
+ * Interface for any class implementing filtering or ranking of tasks.
+ * The list of tasks processed in this way can then be suggested to the user
+ * or used in further TaskSuggesters to combine results.
+ * 
+ * @author sebastian
+ *
+ */
+public interface ITaskSuggester
+{
+	/**
+	 * Get an array of the suggested tasks.
+	 * @return
+	 */
+	public List<Task> getTaskList();
+	
+	/**
+	 * Get an array of the suggested tasks represented as Strings.
+	 * @return an array of task strings
+	 */
+	public String[] getTaskStrings();
+	
+}
