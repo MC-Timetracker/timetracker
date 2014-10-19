d00a7691e1ac8d5ae821fb6a8b48ccf8f70bf954
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-10-19 13:38:30 +0530

    implemented basic start/stop recording tasks

--- src/iiitd/mc/timetracker/MainActivity.java
+++ src/iiitd/mc/timetracker/MainActivity.java
@@ -1,16 +1,22 @@
 package iiitd.mc.timetracker;
 
+import java.util.Date;
 import java.util.List;
 
 import iiitd.mc.timetracker.adapter.CustomArrayAdapter;
 import iiitd.mc.timetracker.context.*;
+import iiitd.mc.timetracker.data.*;
+import iiitd.mc.timetracker.helper.IDatabaseController;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
 /**
  * The Main Activity of the timetracker application
@@ -18,18 +24,23 @@ import android.widget.AutoCompleteTextView;
  *
  */
 public class MainActivity extends ActionBarActivity {
-
+	
+	ITaskSuggestor suggester;
+	TaskService taskService;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		taskService = new TaskService();
+		
 		AutoCompleteTextView autoTv = (AutoCompleteTextView) findViewById(R.id.taskSelectionBox);
 		
-		ITaskSuggestor suggester = new MainTaskSuggestor();
+		suggester = new MainTaskSuggestor();
 		List<String> suggestedTasks = suggester.getTaskStrings();
-		CustomArrayAdapter adapter = new CustomArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, suggestedTasks);
-		autoTv.setAdapter(adapter);
+		CustomArrayAdapter taskListAdapter = new CustomArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, suggestedTasks);
+		autoTv.setAdapter(taskListAdapter);
 		
 		autoTv.setThreshold(0);
 		autoTv.setOnClickListener(new OnClickListener() {
@@ -57,5 +68,46 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	public void onStartRecording(View v)
+	{	
+		// Get the task instance that corresponds to the String entered by the user
+		AutoCompleteTextView taskSelector = (AutoCompleteTextView) findViewById(R.id.taskSelectionBox);
+		final String taskString = taskSelector.getText().toString();
+		
+		taskService.startRecording(taskString);
+		
+		toggleStartStopButton(true);
+		//TODO: implement toggleStartStop through event of TaskService instead.
+		// currently the button state will be wrong if the user cancels the creation of a new task
+	}
+	
+	public void onStopRecording(View v)
+	{
+		taskService.stopRecording();
+		toggleStartStopButton(false);
+	}
+	
+
+	/**
+	 * Change visibility of the Start/Stop buttons.
+	 * @param recording Whether a recording is in progress, resulting in visibility of the "Stop" button.
+	 */
+	public void toggleStartStopButton(boolean recording)
+	{
+		int startVisible = View.VISIBLE;
+		int stopVisible = View.GONE;
+		if(recording)
+		{
+			startVisible = View.GONE;
+			stopVisible = View.VISIBLE;
+		}
+		
+		Button bttStart = (Button) findViewById(R.id.bttStart);
+		Button bttStop = (Button) findViewById(R.id.bttStop);
+		bttStart.setVisibility(startVisible);
+		bttStop.setVisibility(stopVisible);
 	}
 }
