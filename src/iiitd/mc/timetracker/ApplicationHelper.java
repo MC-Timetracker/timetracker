9cc6c1e174ce0ab10d377099d89a5ba4c5370abc
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-10-19 12:41:59 +0530

    added ApplicationHelper class to provide a factory method for DatabaseControllers

---
+++ src/iiitd/mc/timetracker/ApplicationHelper.java
@@ -0,0 +1,39 @@
+package iiitd.mc.timetracker;
+
+import iiitd.mc.timetracker.helper.*;
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Helper class to get the application Context in static functions.
+ * @author sebastian
+ *
+ */
+public class ApplicationHelper extends Application
+{
+    private static Context context;
+
+    public void onCreate(){
+        super.onCreate();
+        ApplicationHelper.context = getApplicationContext();
+    }
+
+    /**
+     * Returns the ApplicationContext.
+     * @return
+     */
+    public static Context getAppContext() {
+        return ApplicationHelper.context;
+    }
+    
+    /**
+     * This DatabaseController factory method creates an instance to use for IDatabaseController.
+     * This helps to easily switch to a mockup controller for debugging.
+     * @return An instance implementing the IDatabaseController interface.
+     */
+    public static IDatabaseController createDatabaseController()
+    {
+    	return new MockupDatabaseController();
+    	//return new DatabaseController(MyApplication.context);
+    }
+}
