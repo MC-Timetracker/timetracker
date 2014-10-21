f966e20251f13383509b74e0bfece1b5090bad9d
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-10-21 19:44:42 +0530

    moved initialization of autocomplete widget to separate function

--- src/iiitd/mc/timetracker/MainActivity.java
+++ src/iiitd/mc/timetracker/MainActivity.java
@@ -36,6 +36,11 @@ public class MainActivity extends ActionBarActivity implements RecorderListener 
 		taskRecorder = new TaskRecorder();
 		taskRecorder.addListener(this);
 		
+		initTaskAutocomplete();
+	}
+	
+	public void initTaskAutocomplete()
+	{
 		AutoCompleteTextView autoTv = (AutoCompleteTextView) findViewById(R.id.taskSelectionBox);
 		
 		suggester = new MainTaskSuggestor();
@@ -52,24 +57,7 @@ public class MainActivity extends ActionBarActivity implements RecorderListener 
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 	
 	
 	public void onStartRecording(View v)
