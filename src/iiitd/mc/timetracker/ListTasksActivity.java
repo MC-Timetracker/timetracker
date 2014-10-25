dc061d0c2417f15093a3e2ea39f89af042c3d4d7
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-10-25 11:59:18 +0530

    added really simple ListView displaying all Tasks in database (no hierarchy)

--- src/iiitd/mc/timetracker/ListTasksActivity.java
+++ src/iiitd/mc/timetracker/ListTasksActivity.java
@@ -1,19 +1,31 @@
 package iiitd.mc.timetracker;
 
+import iiitd.mc.timetracker.data.Task;
+import iiitd.mc.timetracker.helper.IDatabaseController;
+
+import java.util.List;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 public class ListTasksActivity extends BaseActivity {
 	
 	public RelativeLayout relativelayoutlist_task; 
+	ListView lvTasks;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_tasks);
 		navigationDisplay();
+		
+		lvTasks = (ListView) findViewById(R.id.lvTasks);
+		
+		loadTasksList();
 	}
 
 	@Override
@@ -39,6 +51,21 @@ public class ListTasksActivity extends BaseActivity {
 		//brings relative layout of list task to the front on closing the drawer
 		relativelayoutlist_task = (RelativeLayout) findViewById(R.id.relativelayoutlist_task); 
     	relativelayoutlist_task.bringToFront();
-    	
     }
+	
+	
+	/**
+	 * Populate the list in the UI with the Tasks from the database.
+	 */
+	public void loadTasksList()
+	{
+		IDatabaseController db = ApplicationHelper.createDatabaseController();
+		db.open();
+		List<Task> tasks = db.getTasks();
+		db.close();
+		
+		ArrayAdapter<Task> adapter = new ArrayAdapter<Task>(this, 
+				android.R.layout.simple_list_item_1, android.R.id.text1, tasks);
+		lvTasks.setAdapter(adapter);
+	}
 }
