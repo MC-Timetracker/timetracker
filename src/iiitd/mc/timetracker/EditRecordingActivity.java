fbd497329c99479da3bcb4cdfea44d2f73034225
Sebastian Leidig <sebastian.leidig@gmail.com>
2014-11-13 19:35:31 +0530

    added TimePickerDialogs and DatePickerDialogs to edit Recording

--- src/iiitd/mc/timetracker/EditRecordingActivity.java
+++ src/iiitd/mc/timetracker/EditRecordingActivity.java
@@ -3,6 +3,7 @@ package iiitd.mc.timetracker;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +15,8 @@ import iiitd.mc.timetracker.data.Task;
 import iiitd.mc.timetracker.data.TaskRecorderService;
 import iiitd.mc.timetracker.helper.IDatabaseController;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -22,7 +25,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class EditRecordingActivity extends BaseActivity
@@ -61,7 +66,43 @@ public class EditRecordingActivity extends BaseActivity
 		etStartDate = (EditText) findViewById(R.id.editTextStartRecordingDate);
 		etStopTime = (EditText) findViewById(R.id.editTextStopRecordingTime);
 		etStopDate = (EditText) findViewById(R.id.editTextStopRecordingDate);
-		//TODO: show Time/DatePickerDialogs
+		View.OnFocusChangeListener fcDate = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                	//Show your popup here
+                	showDatePicker(v);
+                	//prevent keyboard to start loading, which shifts the dialog up and then down again
+                	v.clearFocus();
+                }
+                else
+                {
+                	//Hide your popup here?
+                }
+            }
+        };
+        View.OnFocusChangeListener fcTime = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                	//Show your popup here
+                	showTimePicker(v);
+                	//prevent keyboard to start loading, which shifts the dialog up and then down again
+                	v.clearFocus();
+                }
+                else
+                {
+                	//Hide your popup here?
+                }
+            }
+        };
+        etStartDate.setOnFocusChangeListener(fcDate);
+        etStopDate.setOnFocusChangeListener(fcDate);
+        etStartTime.setOnFocusChangeListener(fcTime);
+        etStopTime.setOnFocusChangeListener(fcTime);
+        
 		
 		formatTime = android.text.format.DateFormat.getTimeFormat(this);
 		formatDate = android.text.format.DateFormat.getDateFormat(this);
@@ -186,6 +227,82 @@ public class EditRecordingActivity extends BaseActivity
 		adapter = new CustomArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, suggestedTasks);
 		etTaskName.setAdapter(adapter);		
 	}
+	
+	public void showDatePicker(View view)
+	{
+		final EditText editTextRef =(EditText) view;
+		
+		int year = 0;
+		int month = 0;
+		int day = 0;
+		try
+		{
+			Date date = formatDate.parse(editTextRef.getText().toString());
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(date);
+			year = cal.get(Calendar.YEAR);
+			month = cal.get(Calendar.MONTH);
+			day = cal.get(Calendar.DAY_OF_MONTH);
+		} catch (ParseException e)
+		{
+			// ignore
+		}
+		
+		DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener()
+			{			
+				@Override
+				public void onDateSet(DatePicker view, int year,
+						int monthOfYear, int dayOfMonth)
+				{
+					Calendar cal = Calendar.getInstance();
+					cal.set(Calendar.YEAR, year);
+					cal.set(Calendar.MONTH, monthOfYear);
+					cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+					
+					//TODO: check consistency (before/after recording's start/end
+					
+					String date = formatDate.format(cal.getTime());
+					editTextRef.setText(date);
+				}
+			}, year, month, day);
+		dialog.show();
+	}
+	public void showTimePicker(View view)
+	{
+		final EditText editTextRef =(EditText) view;
+		
+		int hour = 0;
+		int minute = 0;
+		try
+		{
+			Date date = formatTime.parse(editTextRef.getText().toString());
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(date);
+			hour = cal.get(Calendar.HOUR_OF_DAY);
+			minute = cal.get(Calendar.MINUTE);
+		} catch (ParseException e)
+		{
+			// ignore
+		}
+		
+		TimePickerDialog dialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener()
+			{			
+				@Override
+				public void onTimeSet(TimePicker picker, int hourOfDay, int minute)
+				{
+					Calendar cal = Calendar.getInstance();
+					cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+					cal.set(Calendar.MINUTE, minute);
+					
+					//TODO: check consistency (before/after recording's start/end
+					
+					String time = formatTime.format(cal.getTime());
+					editTextRef.setText(time);
+				}
+			}, hour, minute, true);
+		dialog.show();
+	}
+	
 	public void onCancel(View view)
 	{
 		finish();
